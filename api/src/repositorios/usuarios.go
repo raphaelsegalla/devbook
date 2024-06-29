626aaa480cfebeb27c72912a7d9f904f26340ec8
raphaelsegalla <raphael.segalla@gmail.com>
2024-06-28 22:56:44 -0300

    implementando busca de usuario por id

--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -63,3 +63,27 @@ func (repositorio usuarios) Buscar(nomeOuNick string) ([]modelos.Usuario, error)
 
 	return usuarios, nil
 }
+
+func (repositorio usuarios) BuscarPorId(usuarioId uint64) (modelos.Usuario, error) {
+	linhas, erro := repositorio.db.Query("select id, nome, nick, email, criadoEm from usuarios where id = ?", usuarioId)
+	if erro != nil {
+		return modelos.Usuario{}, erro
+	}
+	defer linhas.Close()
+
+	var usuario modelos.Usuario
+
+	if linhas.Next() {
+		if erro = linhas.Scan(
+			&usuario.ID,
+			&usuario.Nome,
+			&usuario.Nick,
+			&usuario.Email,
+			&usuario.CriadoEm,
+		); erro != nil {
+			return modelos.Usuario{}, erro
+		}
+	}
+
+	return usuario, nil
+}
