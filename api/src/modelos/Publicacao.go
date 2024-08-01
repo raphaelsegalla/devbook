5a8a441b760b64b614756d2b686af7d9d9a3d3e4
raphaelsegalla <raphael.segalla@gmail.com>
2024-08-01 20:54:20 -0300

    implementando validacao de publicacoes

--- api/src/modelos/Publicacao.go
+++ api/src/modelos/Publicacao.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type Publicacao struct {
 	ID        uint64    `json:"id,omitempty"`
@@ -10,4 +14,31 @@ type Publicacao struct {
 	AutorNick uint64    `json:"autorNick,omitempty"`
 	Curtidas  uint64    `json:"curtidas"`
 	CriadaEm  time.Time `json:"criadadEm,omitempty"`
+}
+
+func (publicacao *Publicacao) Preparar() error {
+	if erro := publicacao.validar(); erro != nil {
+		return erro
+	}
+
+	publicacao.formatar()
+
+	return nil
+}
+
+func (publicacao *Publicacao) validar() error {
+	if publicacao.Titulo == "" {
+		return errors.New("O título é obrigatório e nâo pode estar me branco")
+	}
+
+	if publicacao.Conteudo == "" {
+		return errors.New("O conteúdo é obrigatório e nâo pode estar me branco")
+	}
+
+	return nil
+}
+
+func (publicacao *Publicacao) formatar() {
+	publicacao.Titulo = strings.TrimSpace(publicacao.Titulo)
+	publicacao.Conteudo = strings.TrimSpace(publicacao.Conteudo)
 }
